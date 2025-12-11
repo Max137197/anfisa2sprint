304a537f46a456fba37b14f8e6cd6e0593f06b78
Max Gulin <gulin_1371@mail.ru>
2025-12-11 17:53:50 +0700

    s

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,9 +4,10 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(default=100,
+        verbose_name='Порядок отображения')
     
     class Meta:
         verbose_name = 'категория'
