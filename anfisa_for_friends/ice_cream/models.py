98951118eb55b12618d18b5d216bc4d927750a21
Max Gulin <gulin_1371@mail.ru>
2025-12-11 17:56:45 +0700

    ы

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,43 +1,53 @@
 from django.db import models
-
 from core.models import PublishedModel
 
 
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
-    output_order = models.PositiveSmallIntegerField(default=100,
-        verbose_name='Порядок отображения')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
     
     class Meta:
         verbose_name = 'категория'
-        verbose_name_plural = 'Категории' 
+        verbose_name_plural = 'Категории'
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, verbose_name='Упаковка')
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    title = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='Упаковка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='Категория'
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
-
+    toppings = models.ManyToManyField(
+        Topping,
+        verbose_name='Топпинги',
+        blank=True
+    )
+    is_on_main = models.BooleanField(
+        default=False,
+        verbose_name='На главной',
+        help_text='Отображать на главной странице'
+    )
