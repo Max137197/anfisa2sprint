01e2c6be1fef3099e4e89ded9abee3b1d1b58ea4
Max Gulin <gulin_1371@mail.ru>
2025-12-11 17:46:38 +0700

    S

--- anfisa_for_friends/about/apps.py
+++ anfisa_for_friends/about/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AboutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'about'
+    name = 'ice_cream'
+    verbose_name = 'Каталог мороженого' 
