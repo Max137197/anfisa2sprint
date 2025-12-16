a7c7c38a5857ab961a84150ef2cc63987b1fc423
Max Gulin <gulin_1371@mail.ru>
2025-12-16 17:48:42 +0700

    в

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -10,9 +10,3 @@ urlpatterns = [
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
 ]
-
-# Если проект запущен в режиме разработки...
-if settings.DEBUG:
-    import debug_toolbar
-    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
